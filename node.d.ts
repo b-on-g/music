854001734a6095b8bd3984e28e9fedb21d209f38
Dev-cmyser <Dev-cmyser@users.noreply.github.com>
2026-08-24 07:54:45 +0000

    Deploying to gh-pages from @ b-on-g/music@dcb71d62fe8712837e851992519dda53a823b22b 🚀

--- node.d.ts
+++ node.d.ts
@@ -1924,24 +1924,44 @@ declare namespace $ {
 }
 
 declare namespace $ {
-    let $mol_mem_persist: typeof $mol_wire_solid;
-}
-
-declare namespace $ {
-    let $mol_mem_cached: typeof $mol_wire_probe;
-}
-
-declare namespace $ {
-    function $mol_wait_user_async(this: $): Promise<unknown>;
-    function $mol_wait_user(this: $): unknown;
-}
-
-declare namespace $ {
     class $mol_storage extends $mol_object2 {
-        static native(): StorageManager;
-        static persisted(next?: boolean, cache?: 'cache'): boolean;
-        static estimate(): StorageEstimate;
-        static dir(): FileSystemDirectoryHandle;
+        /** Is storage a long term. */
+        static persisted(next?: boolean): boolean;
+        /** Total storage quota in bytes. */
+        static total(): number;
+        /** Total storage usage in bytes. */
+        static used(): number;
+        /** Minimum available free space in bytes. */
+        static free(): number;
+        /** Fulfillness of storage. */
+        static portion(): number;
+        /**
+         * Fulfillness logarithmic level.
+         * `0` - empty
+         * `1` - half free
+         * `2` - quart free
+         * `Infinity` - fulfilled
+         */
+        static level(): number;
+    }
+}
+
+declare namespace $ {
+    /** State of time moment */
+    class $mol_state_time extends $mol_object {
+        static task(precision: number, reset?: null): $mol_after_timeout | $mol_after_frame;
+        static now(precision: number): number;
+    }
+}
+
+declare namespace $ {
+    class $mol_storage_node extends $mol_storage {
+        static persisted(): boolean;
+        static stats(): import("node:fs").StatsFs;
+        static total(): number;
+        static used(): number;
+        static free(): number;
+        static portion(): number;
     }
 }
 
@@ -1974,6 +1994,10 @@ declare namespace $ {
         wait(): Promise<() => void>;
         grab(): () => void;
     }
+}
+
+declare namespace $ {
+    let $mol_mem_cached: typeof $mol_wire_probe;
 }
 
 declare namespace $ {
@@ -3229,14 +3253,6 @@ declare namespace $.$$ {
 }
 
 declare namespace $ {
-}
-
-declare namespace $ {
-    /** State of time moment */
-    class $mol_state_time extends $mol_object {
-        static task(precision: number, reset?: null): $mol_after_timeout | $mol_after_frame;
-        static now(precision: number): number;
-    }
 }
 
 declare namespace $ {
@@ -23517,6 +23533,7 @@ declare namespace $ {
             readonly Cpu_system: (auto?: any) => $giper_baza_stat_ranges | null;
             readonly Mem_used: (auto?: any) => $giper_baza_stat_ranges | null;
             readonly Mem_free: (auto?: any) => $giper_baza_stat_ranges | null;
+            readonly Fs_used: (auto?: any) => $giper_baza_stat_ranges | null;
             readonly Fs_free: (auto?: any) => $giper_baza_stat_ranges | null;
             readonly Fs_reads: (auto?: any) => $giper_baza_stat_ranges | null;
             readonly Fs_writes: (auto?: any) => $giper_baza_stat_ranges | null;
@@ -24186,6 +24203,8 @@ declare namespace $ {
             readonly Mem_used: typeof $giper_baza_stat_ranges;
             /** Memory in MB */
             readonly Mem_free: typeof $giper_baza_stat_ranges;
+            /** FS used */
+            readonly Fs_used: typeof $giper_baza_stat_ranges;
             /** FS free */
             readonly Fs_free: typeof $giper_baza_stat_ranges;
             /** FS read count */
