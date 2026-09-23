2206472251ba80557917010aa283996169cd6152
Dev-cmyser <Dev-cmyser@users.noreply.github.com>
2026-09-23 12:32:18 +0000

    Deploying to gh-pages from @ b-on-g/music@adc40b65d471b01e68a8889163852385dda79f42 🚀

--- node.js
+++ node.js
@@ -16727,6 +16727,8 @@ var $;
             for (const gift of this._gift.values()) {
                 if (mine.units_persisted.has(gift))
                     continue;
+                if (!this.unit_seal(gift))
+                    continue;
                 persisting.add(gift);
                 check_lord(gift.lord());
                 check_lord(gift.mate());
@@ -16735,6 +16737,8 @@ var $;
                 for (const units of kids.values()) {
                     for (const sand of units.values()) {
                         if ($mol_wire_sync(mine.units_persisted).has(sand))
+                            continue;
+                        if (!this.unit_seal(sand))
                             continue;
                         persisting.add(sand);
                         check_lord(sand.lord());
